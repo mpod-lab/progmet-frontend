36051d75b69841cf8c12e8e2d16101a3e83eb2a3
mpod-lab <martynapodsiadly@gmail.com>
2019-12-29 15:36:01 +0100

    components footer main navbar categories

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainComponent } from './components/main/main.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { ProductComponent } from './components/product/product.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { ProductCategoryComponent } from './components/product-category/product-category.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    ContactComponent,
+    ProductComponent,
+    FooterComponent,
+    ProductCategoryComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
